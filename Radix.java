ad42568eb03a9aefea6ec995e6e0cf928c2093d0
Justin Liu <exovitejl@gmail.com>
2021-01-11 17:17:23 -0500

    wrote method merge

--- Radix.java
+++ Radix.java
@@ -1,4 +1,3 @@
-//import Math;
 public class Radix{
 /*
 get nth digit of an int, where 0 is the ones column, 1 is the tens column etc.
@@ -24,12 +23,17 @@ length(5112) -> 4
   }
 
 /*
-This requires your MyLinkedList file to be present locally, but please do not commit this extra file.
-Merge all of the linked lists in the bucket array into your original linked list. The original may have elements, and those should be kept.
+This requires your MyLinkedList file to be present locally, but please do not
+commit this extra file.
+Merge all of the linked lists in the bucket array into your original linked list.
+The original may have elements, and those should be kept.
 This is O(buckets.length) which should be 10 when we use this later.
 The bucket[0] list will be merged first, then the bucket[1] etc.
 */
-//  public static void merge(MyLinkedList original,MyLinkedList[]buckets){
+  public static void merge(MyLinkedList original,MyLinkedList[]buckets){
+    for(int i=0;i<buckets.length;i++){
+      original.extend(buckets[i]);
+    }
+  }
 
-//  }
 }
